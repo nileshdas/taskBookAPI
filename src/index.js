3ac838f3b7a82ade76ac8695afc15c2390aa3a12
Nilesh Das <nileshdas50@gmail.com>
2020-08-08 15:34:34 +0530

    Resource CRUD endpoints

--- src/index.js
+++ src/index.js
@@ -68,6 +68,18 @@ app.patch('/users/:id', async (req, res) =>{
   }
 })
 
+app.delete('/users/:id', async (req, res) => {
+  try{
+    const user = await User.findByIdAndDelete(req.params.id)
+    if(!user){
+      return res.status(404).send()
+    }
+    res.send(user)
+  }catch(e){
+    res.status(500).send()
+  }
+})
+
 app.post('/tasks', async (req, res) =>{
    const task = new Task(req.body)
    try {
@@ -98,6 +110,40 @@ app.get('/tasks/:id', async (req, res) => {
     }
     res.send(task)
   } catch(e){
+    res.status(500).send()
+  }
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+  const updates = Object.keys(req.body)
+  const allowedUpdates = ['description', 'completed']
+  const isValidOperation = updates.every((update) => {
+    return allowedUpdates.includes(update)
+
+  })
+  if(!isValidOperation){
+    return res.status(400).send('Invalid Updates')
+  }
+  try{
+    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new:true, runValidators: true})
+
+    if(!task){
+      res.status(404).send()
+    }
+    res.send(task)
+  }catch(e){
+    res.status(400).send(e)
+  }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+  try{
+    const task = await Task.findByIdAndDelete(req.params.id)
+    if(!task){
+      return res.status(404).send()
+    }
+    res.send(task)
+  }catch(e){
     res.status(500).send()
   }
 })
