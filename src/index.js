3ae6f5376f77bc8a1f4125d2be51c5567c971b3b
Nilesh Das <nileshdas50@gmail.com>
2020-08-10 01:50:19 +0530

    Relationship established between User and Tasks

--- src/index.js
+++ src/index.js
@@ -11,12 +11,18 @@ app.use(userRouter)
 app.use(taskRouter)
 
 
-
-
-
-
-
-
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task');
+const User = require('./models/user')
+
+const main = async () => {
+  const user = await User.findById('5f3049f92f5125262c9b686b')
+
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks);
+}
+
+main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -70,7 +70,7 @@ router.get('/users/:id', async (req, res) => {
 
 })
 
-router.patch('/users/:id', async (req, res) =>{
+router.patch('/users/me', auth, async (req, res) =>{
   const updates = Object.keys(req.body)
   const allowedUpdates = ['name', 'email', 'password', 'age']
   const isValidOperation = updates.every((update) => {
@@ -81,29 +81,25 @@ router.patch('/users/:id', async (req, res) =>{
     return res.status(400).send('Invalid Updates')
   }
   try {
-    const user = await User.findById(req.params.id)
-    updates.forEach((update) => {
-      user[update] = req.body[update]
-    })
-    await user.save()
-    //const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
 
-    if(!user){
-      return res.status(404).send()
-    }
-    res.send(user)
+    updates.forEach((update) => {
+      req.user[update] = req.body[update]
+    })
+    await req.user.save()
+
+    res.send(req.user)
   } catch(e){
     res.status(400).send(e)
   }
 })
 
-router.delete('/users/:id', async (req, res) => {
+router.delete('/users/me', auth,  async (req, res) => {
   try{
-    const user = await User.findByIdAndDelete(req.params.id)
-    if(!user){
-      return res.status(404).send()
-    }
-    res.send(user)
+    const user = await User.findByIdAndDelete(req.user._id)
+
+    await req.user.remove()
+
+    res.send(req.user)
   }catch(e){
     res.status(500).send()
   }
